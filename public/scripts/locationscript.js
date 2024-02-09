6060068a2e74fc523147cbe745b58ca9f6103e41
george <70286749+GeorgeEllis123@users.noreply.github.com>
2024-02-09 13:39:06 -0500

    Made it so long and lat are displayed on website

--- public/scripts/locationscript.js
+++ public/scripts/locationscript.js
@@ -11,11 +11,15 @@ function successCallback(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
+    document.getElementById("long").innerHTML = longitude;
+    document.getElementById("lat").innerHTML = latitude;
     console.log("latitude: " + latitude);
     console.log("longitude: " + longitude);
 }
 
 // Error callback function
 function errorCallback(error) {
+    document.getElementById("long").innerHTML = "Location";
+    document.getElementById("lat").innerHTML = "Not Recieved";
     console.error("Error getting user's location:", error);
 }
