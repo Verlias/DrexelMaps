16ddcc41ba3c8acb29d1eb7f44cb69498ae267e3
george <70286749+GeorgeEllis123@users.noreply.github.com>
2024-02-21 12:59:08 -0500

    Created test route on the map

--- server/public/scripts/mapscript.js
+++ server/public/scripts/mapscript.js
@@ -1,3 +1,56 @@
+// Node class
+class Node {
+    constructor(name, position, connections) {
+        this.name = name;               // String (e.g. "Korman Center 111")
+        this.position = position;       // Tuple of floats (x,y)
+        this.connections = connections; // Array of Node objects
+        this.heuristic = 9999;          // Used to track order in the queue
+        this.previous = null;           // Node object (previous connection in A*)
+    }
+    get x() {
+        return this.position[0];
+    }
+
+    get y() {
+        return this.position[1];
+    }
+
+    setPrevious(node) {
+        this.previous = node;
+    }
+
+    getShifted(offsetX, offsetY, scale) {
+        let newX = Math.floor((this.x * scale + offsetX));
+        let newY = Math.floor((this.y * scale + offsetY));
+        return [newX, newY]
+    }
+
+    drawCircle(ctx, offsetX, offsetY, scale) {
+        const newPos = this.getShifted(offsetX, offsetY, scale)
+        ctx.beginPath();
+        ctx.arc(newPos[0], newPos[1], 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath()
+    }
+
+    drawPath(ctx, offsetX, offsetY, scale) {
+        if (this.previous != null) {
+            let fromPos = this.getShifted(offsetX, offsetY, scale)
+            let toPos = this.previous.getShifted(offsetX, offsetY, scale)
+            ctx.beginPath();
+            ctx.moveTo(fromPos[0], fromPos[1]);
+            ctx.lineTo(toPos[0], toPos[1]);
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 3;
+            ctx.stroke();
+            ctx.closePath()
+            this.previous.drawPath(ctx, offsetX, offsetY, scale)
+        }
+    }
+}
+
+// Canvas
 const canvas = document.getElementById('mapCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -14,15 +67,26 @@ let offsetX = -100;
 let offsetY = -500;
 let scale = .5;
 
+// Nodes
+testnode = new Node("Korman Center", [1460, 1755], []);
+testnode2 = new Node("Disque Hall", [1540, 1790], []);
+testnode3 = new Node("Lebow Hall", [1520, 1720], []);
+testnode.setPrevious(testnode2)
+testnode2.setPrevious(testnode3)
+
 // Draw map image
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(mapImage, offsetX, offsetY, mapImage.width * scale, mapImage.height * scale);
+    testnode.drawPath(ctx, offsetX, offsetY, scale);
+    testnode.drawCircle(ctx, offsetX, offsetY, scale);
+    //testnode2.drawCircle(ctx, offsetX, offsetY, scale);
+    testnode3.drawCircle(ctx, offsetX, offsetY, scale);
 }
 
 // Handle arrow keys for panning
 document.addEventListener('keydown', e => {
-    const panSpeed = 10;
+    const panSpeed = 15;
     switch (e.key) {
         case 'ArrowUp':
             if (offsetY + panSpeed < 0)
---
+++ server/public/scripts/priorityqueue.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+class PriorityQueue {
+    constructor(start, end) {
+        this.start = start;     // Start Node
+        this.end = end;         // End Node
+        this.queue = [];        // Array of Nodes
+    }
+}
