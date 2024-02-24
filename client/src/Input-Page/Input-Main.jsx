4c30768795c67e83406bee27e0f7ef53898c3778
Logan <loganvoravong@gmail.com>
2024-02-23 23:09:37 -0500

    Added form functionality to the input.jsx file

--- client/src/Input-Page/Input-Main.jsx
+++ client/src/Input-Page/Input-Main.jsx
@@ -1,23 +1,56 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import styles from "./Input-Page.module.css";
 
 
 function InputMain(){
+    const navigate = useNavigate();
+
+    function fillinput(event) {
+        var inputbox = document.getElementById('destination');
+        var autofillValue = event.target.innerText;
+        inputbox.value = autofillValue;
+    }
+
+    function submitinput() {
+        // Get the input text
+        var inputText = document.getElementById('destination').value;
+
+        const valid = Boolean(validateinput(inputText));
+
+        if (valid) {
+            navigate('/Map');
+            console.log("User sent to new Page!")
+        }
+    }
+
+    function validateinput(input) {
+        // Validation will check if the class is in the database
+        if (["Disque Hall 108", "Randell Hall 120", "Lebow Engineering Center 134", "Korman Center 111"].includes(input)) {
+            document.getElementById("msg").innerHTML = "Input Recieved!"
+            return true
+        }
+        else {
+            document.getElementById("msg").innerHTML = "Please Input a Valid Location"
+            return false
+        }
+    }
 
     return(
         <>
-           <form className={styles.form}>
-                <label className={styles.label} for="destination">Class Location:</label>
-                <input className={styles.input} id="destination" name="destination" required/>
-                <button className={styles.autofill} type="button" onclick="fillinput(this)">Disque Hall 108</button>
-                <button className={styles.autofill} type="button" onclick="fillinput(this)">Randell Hall 120</button>
-                <button className={styles.autofill} type="button" onclick="fillinput(this)">Lebow Engineering Center 134</button>
-                <button className={styles.autofill} type="button" onclick="fillinput(this)">Korman Center 111</button>
+           <form className={styles.form} id="textForm" >
+                <label className={styles.label} htmlFor="destination">Class Location:</label>
+                <input className={styles.input} id="destination" name="destination" required />
+                <button className={styles.autofill} type="button" onClick={fillinput}>Disque Hall 108</button>
+                <button className={styles.autofill} type="button" onClick={fillinput}>Randell Hall 120</button>
+                <button className={styles.autofill} type="button" onClick={fillinput}>Lebow Engineering Center 134</button>
+                <button className={styles.autofill} type="button" onClick={fillinput}>Korman Center 111</button>
                 <p id="msg"></p>
-                <button className={styles.submit} type="button" onclick="submitinput()">Submit</button>
-            </form> 
+                <button className={styles.submit} type="button" onClick={submitinput}>Submit</button>
+            </form>
         </>
     )
 };
 
 export default InputMain;
+
