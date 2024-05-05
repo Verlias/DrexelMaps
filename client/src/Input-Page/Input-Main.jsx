217ed92d67e7ed9a6c2e945afb37833b077d84d4
Alvin Chen <ac4336.drexel.edu>
2024-05-05 14:20:54 -0400

    Added another useEffect hook reponsible for setting starting location and ending location suggestions, updated existing useEffect hook to fetch save classes data via fetchClassData function, added a new function to filter out duplicate buildings in saved classes data.

--- client/src/Input-Page/Input-Main.jsx
+++ client/src/Input-Page/Input-Main.jsx
@@ -10,24 +10,64 @@ function InputMain() {
     const [endingLocationInput, setEndingLocationInput] = useState("");
     const [startingLocationSuggestions, setStartingLocationSuggestions] = useState([]);
     const [endingLocationSuggestions, setEndingLocationSuggestions] = useState([]);
+    const [savedClasses, setSavedClasses] = useState([]);
+    const token = localStorage.getItem('token');
     
     useEffect(() => {
-        // Fetching building names from JSON data
-        const names = buildingData.map(building => building.name).filter(name => name !== 'road').sort();
-        setStartingLocationSuggestions(names);
-        setEndingLocationSuggestions(names);
+        // Fetch saved classes data
+        if (token) fetchClassesData();
     }, []);
+
+    // Run this side code only when the savedClasses state has changed.
+    useEffect(() => {
+        // If the user is logged in, set starting location and ending location
+        // suggestions to unique buildings in the saved classes data.
+        if (token){
+            setStartingLocationSuggestions(filterClassesData(savedClasses));
+            setEndingLocationSuggestions(filterClassesData(savedClasses));
+        }
+        // If the user is not logged in, set starting location and ending location
+        // suggestions to all the buildings in the building_locations.json data.
+        else {
+            // Fetching building names from building_locations.json JSON data
+            const names = buildingData.map(building => building.name).filter(name => name !== 'road').sort();
+            setStartingLocationSuggestions(names);
+            setEndingLocationSuggestions(names);
+        }
+    }, [savedClasses])
+
+    // Gets the saved classes
+    const fetchClassesData = async () => {
+        try {
+            // Fetch user saved classes data from the backend
+            const response = await axios.get('http://localhost:3000/api/saved');
+            // Set the saved classes data in state
+            setSavedClasses(response.data);
+            console.log(filterClassesData(response.data));
+        } catch (error) {
+            console.error("Error fetching saved classes:", error);
+        }
+    };
+
+    // Filter saved class data fetched from /api/saved endpoint by returning
+    // an array that only contain unique buildings where the saved classes are located.
+    const filterClassesData = (classes) => {
+        return [...new Set(classes.map(savedClass => savedClass.building))];
+    }
 
     const handleInputChange = (event, setValue, setSuggestions) => {
         const { value } = event.target;
         setValue(value);
+
         if (value.trim() === '') {
-            setSuggestions(buildingData.map(building => building.name).filter(name => name !== 'road').sort());
+            setSuggestions(buildingData.map(building => building.name).filter((name) => name !== 'road').sort());
             return;
         }
+
         const filteredSuggestions = buildingData
             .map(building => building.name)
-            .filter(name => name.toLowerCase().startsWith(value.toLowerCase()));
+            .filter((name) => (name.toLowerCase().startsWith(value.toLowerCase())) && (name !== 'road'))
+            .sort();
         setSuggestions(filteredSuggestions);
     };
 
@@ -35,7 +75,6 @@ function InputMain() {
         setValue(value);
         setSuggestions([]);
     };
-
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -52,7 +91,7 @@ function InputMain() {
             // Handle error, show message to user, etc.
         }
     };
-    
+
 
     return (
         <>
